bb217998868d39a1174d08c6d261e79dbdd267cc
jmiddend <jmiddend@c9c54f97-0e0a-0b51-7e23-1bce3dc07db9>
2010-12-09 04:09:29 +0000

    Update version stamp to 1.0.2 (fixes issue 135)

--- src/defines.h
+++ src/defines.h
@@ -40,14 +40,14 @@
 /*
  * Current version string
  */
-#define VERSION_STRING   "1.0.1"
+#define VERSION_STRING   "1.0.2b1"
 
 /*
  * Current (Sangband) version numbers
  */
 #define VERSION_MAJOR   1
 #define VERSION_MINOR   0
-#define VERSION_PATCH   1
+#define VERSION_PATCH   2
 
 /*
  * This value is used for beta versions (now deprecated)
